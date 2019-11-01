27cd639695bf7870d9ab0527e7563513815a723b
Joey <jocelynh@middlebury.edu>
2019-11-01 02:51:34 -0400

    Displays form component but breaks once you click a new value for each prop.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
 
 /* eslint-disable react/prefer-stateless-function */
 function App() {
+  const [permitType, setPermit] = useState(false);
+  const [userType, setUser] = useState(false);
+  const [timeIn, setTimeIn] = useState(false);
+  const [timeOut, setTimeOut] = useState(false);
+  const [clicked, setButton] = useState(false);
+  // state instantiations need to happen inside of the exported component
+  // might want to define these in App.js, and then pass them as props to this component.
+  //first have to create all the props we have created
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to your CS312 Project</h1>
+        <h1 className="App-title">Welcome beta Panther Parking</h1>
       </header>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
-      <Form />
+      <Form
+        permitType={setPermit}
+        userType={setUser}
+        timeIn={setTimeIn}
+        timeOut={setTimeOut}
+      />
     </div>
   );
 }
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,30 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const DropDownSelect = styled.select`
   font-size: 12px;
 `;
 
-const DropDown = props => {
-  // state instantiations need to happen inside of the exported component
-  // might want to define these in App.js, and then pass them as props to this component.
-  //first have to create all the props we have created
-  const [permitType, setpermit] = useState(false);
-  //const [userType, setUser] = useState(false);
-  //const [timeIn, setTimeIn] = useState(false);
-  //const [timeOut, setTimeOut] = useState(false);
+const DropDown = ({
+  permitType,
+  setPermit,
+  userType,
+  setUser,
+  timeIn,
+  setTimeIn,
+  timeOut,
+  setTimeOut,
+  clicked,
+  setButton
+}) => {
   return (
-    <DropDownSelect
-      value={permitType}
-      onChange={event => {
-        alert(event.target.value); // just to test it. Change this to whatever you want.
-      }}
-    >
-      <option value="x">PermitX</option>
-      <option value="y">PermitY</option>
-      <option value="z">PermitZ</option>
-    </DropDownSelect>
+    <div>
+      <h3>Please select your permit type:</h3>
+
+      <DropDownSelect
+        value={permitType}
+        onChange={event => {
+          setPermit(event.target.value);
+          console.log(permitType); // just to test it. Change this to whatever you want.
+        }}
+      >
+        <option value="x">PermitX</option>
+        <option value="y">PermitY</option>
+        <option value="z">PermitZ</option>
+      </DropDownSelect>
+
+      <h3>Please select your user type:</h3>
+
+      <DropDownSelect
+        value={userType}
+        onChange={event => {
+          setUser(event.target.value);
+          console.log(userType); // just to test it. Change this to whatever you want.
+        }}
+      >
+        <option value="admin">Aministration</option>
+        <option value="student">Student</option>
+        <option value="visitor">Visitor</option>
+      </DropDownSelect>
+
+      <h3>Please select time you wish to park:</h3>
+
+      <DropDownSelect
+        value={timeIn}
+        onChange={event => {
+          setTimeIn(event.target.value);
+          console.log(timeIn); // just to test it. Change this to whatever you want.
+        }}
+      >
+        <option value="1:00AM">1:00AM</option>
+        <option value="1:30AM">1:30AM</option>
+        <option value="12:00AM">12:00AM</option>
+      </DropDownSelect>
+
+      <h3>Please select a time you wish to leave:</h3>
+
+      <DropDownSelect
+        value={timeOut}
+        onChange={event => {
+          setTimeOut(event.target.value);
+          console.log(timeOut); // just to test it. Change this to whatever you want.
+        }}
+      >
+        <option value="1:00AM">1:00AM</option>
+        <option value="1:30AM">1:30AM</option>
+        <option value="12:00AM">12:00AM</option>
+      </DropDownSelect>
+
+      <h3></h3>
+
+      <button onclick={() => setButton(true)}>FIND PARKING!</button>
+    </div>
   );
 };
 
