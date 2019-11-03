bd39401a41ee57190925c890c7bf92af4ef1787a
Joey <jocelynh@middlebury.edu>
2019-11-03 15:04:49 -0500

    Updated package.json/package-lock.json. w/ moment

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome beta Panther Parking</h1>
+        <h1 className="App-title">Welcome to beta Panther Parking 1.0</h1>
       </header>
       <p className="App-intro">Ready to find parking?</p>
       <Form
