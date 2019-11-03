385b9fe572e8edf0953e621dd63f1752c9611b46
aumitleon <aumitleon1@gmail.com>
2019-11-03 11:31:57 -0500

    load API key and dataset via custom API

--- client/src/components/ParkingMap.js
+++ client/src/components/ParkingMap.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import mapboxgl from 'mapbox-gl';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
 
-export function ParkingMap() {
+const ParkingMap = props => {
   const [mapState, setMapState] = useState({
     viewport: {
       width: 400,
@@ -14,32 +14,38 @@ export function ParkingMap() {
     }
   });
 
-  const token = 'SECRET';
+  const [key, setKey] = useState('');
+  fetch('/api/map/key')
+    .then(response => response.text())
+    .then(body => {
+      setKey(body);
+    })
+    .catch(err => console.log(err));
 
-  return (
-    <ReactMapGL
-      {...mapState.viewport}
-      mapboxApiAccessToken={token}
-      onViewportChange={viewport => setMapState({ viewport })}
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-    >
-      <Source
-        id="my-data"
-        type="geojson"
-        data="https://api.mapbox.com/datasets/v1/aumitleon/ck2dh7hch0cg32qpmddggahmm/features?access_token=pk.eyJ1IjoiYXVtaXRsZW9uIiwiYSI6ImNrMjlhNW0wejBzNHozYm1zMmVhNndnb2kifQ.jek8UQQ1GROayhoYE_Xt1A"
+  if (key !== '' && props.dataSet !== undefined) {
+    return (
+      <ReactMapGL
+        {...mapState.viewport}
+        mapboxApiAccessToken={key}
+        onViewportChange={viewport => setMapState({ viewport })}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
       >
-        <Layer
-          id="point"
-          type="fill"
-          paint={{
-            'fill-outline-color': '#484896',
-            'fill-color': '#6e599f',
-            'fill-opacity': 0.75
-          }}
-        />
-      </Source>
-    </ReactMapGL>
-  );
-}
+        <Source id="my-data" type="geojson" data={props.dataSet}>
+          <Layer
+            id="point"
+            type="fill"
+            paint={{
+              'fill-outline-color': '#484896',
+              'fill-color': '#6e599f',
+              'fill-opacity': 0.75
+            }}
+          />
+        </Source>
+      </ReactMapGL>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
+};
 
 export default ParkingMap;
