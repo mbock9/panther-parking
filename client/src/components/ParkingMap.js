65e9934a2158af76fee5c31f4ecaa498692eb150
ben vandenbosch <benjaminrvandenbosch@gmail.com>
2019-11-04 10:29:13 -0500

    add map functionality

--- client/src/components/ParkingMap.js
+++ client/src/components/ParkingMap.js
@@ -4,23 +4,51 @@ import mapboxgl from 'mapbox-gl';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
 
 const ParkingMap = props => {
+  // Represent the viewport for the map as a state. Pass the setting function
+  // to Mapbox as the callback for changes to viewport
   const [mapState, setMapState] = useState({
     viewport: {
       width: 400,
       height: 400,
       latitude: 44.0082,
       longitude: -73.176,
-      zoom: 15
+      zoom: 13
     }
   });
 
-  const [key, setKey] = useState('');
+  const [key, setKey] = useState(''); // Represent MapBox API Key as state
+
+  // Fetch and set the API key from server (saved in the .env)
   fetch('/api/map/key')
     .then(response => response.text())
     .then(body => {
       setKey(body);
     })
     .catch(err => console.log(err));
+
+  // Example using a student permit to filter on (this info will be passed in from form)
+  let permType = 'sPermit';
+
+  // Create geojson skeletons for lots that match/don't match filter criteria
+  let parkable = { features: [], type: 'FeatureCollection' };
+  let nonParkable = { features: [], type: 'FeatureCollection' };
+
+  // Iterate through lot data and update lists accordingly
+  if (props.dataSet.features != undefined) {
+    if (permType) {
+      props.dataSet.features.forEach(feature => {
+        if (feature.properties[permType].toUpperCase() === 'TRUE') {
+          parkable.features.push(feature);
+        } else {
+          nonParkable.features.push(feature);
+        }
+      });
+    }
+    // If no filters are defined, render all lots as parkable
+    else {
+      parkable = props.dataSet;
+    }
+  }
 
   if (key !== '' && props.dataSet !== undefined) {
     return (
@@ -30,13 +58,24 @@ const ParkingMap = props => {
         onViewportChange={viewport => setMapState({ viewport })}
         mapStyle="mapbox://styles/mapbox/streets-v11"
       >
-        <Source id="my-data" type="geojson" data={props.dataSet}>
+        <Source id="parkable-regions" type="geojson" data={parkable}>
           <Layer
-            id="point"
+            id="parkable"
             type="fill"
             paint={{
-              'fill-outline-color': '#484896',
-              'fill-color': '#6e599f',
+              'fill-outline-color': '#105e01',
+              'fill-color': '#1fb302',
+              'fill-opacity': 0.75
+            }}
+          />
+        </Source>
+        <Source id="nonparkable-regions" type="geojson" data={nonParkable}>
+          <Layer
+            id="non-parkable"
+            type="fill"
+            paint={{
+              'fill-outline-color': '#960014',
+              'fill-color': '#f51836',
               'fill-opacity': 0.75
             }}
           />
