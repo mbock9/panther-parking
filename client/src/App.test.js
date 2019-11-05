165e4085bb046e2a52c26cad0913a3032b37b653
Miles Bock <mbock@middlebury.edu>
2019-11-04 22:48:07 -0500

    init commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+
 import App from './App';
+import Form from './components/Form';
+import ParkingMap from './components/ParkingMap';
+
+describe('App shallow rendering tests', () => {
+  let app;
+
+  beforeEach(() => {
+    app = shallow(<App />);
+  });
+
+  describe('App component initial content', () => {
+    test('Has title', () => {
+      // toContainMatchingElement doesn't seem to work with JSX
+      expect(
+        app.containsMatchingElement(
+          <h1 className="App-title">Welcome to beta Panther Parking 1.0</h1>
+        )
+      ).toBe(true);
+    });
+
+    test('Contains a form component', () => {
+      expect(app).toContainExactlyOneMatchingElement(Form);
+    });
+
+    test('Contains a parking map component', () => {
+      expect(app.toContainMatchingElement(ParkingMap));
+    });
+  });
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
---
+++ client/src/components/Form.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Form from './Form';
+
+const testCase1 = {
+  permitType: 'x',
+  userType: 'student',
+  timeIn: '1:00AM',
+  timeOut: ''
+};
+
+const testCase2 = {
+  permitType: 's',
+  userType: 'student',
+  timeIn: '1:00AM',
+  timeOut: ''
+};
+
+describe('Form tests', () => {
+  let form;
+
+  beforeEach(() => {
+    form = mount(
+      <Form
+        permitType={testCase1.permitType}
+        setPermit={jest.fn}
+        userType={testCase1.userType}
+        setUser={jest.fn}
+        timeIn={testCase1.timeIn}
+        setTimeIn={jest.fn}
+        timeOut={testCase1.timeOut}
+        setTimeOut={jest.fn}
+      />
+    );
+  });
+
+  test('Renders form', () => {
+    const formItems = form.find(Form).find('select');
+    console.log(formItems.filterWhere(n => n).debug());
+    expect(
+      formItems
+        .filterWhere(n => {
+          n.value === 'student';
+        })
+        .text()
+        .toEqual(testCase1.permitType)
+    );
+    // expect(formItems.map(select => select.text())).toEqual(
+    //   toSections(sampleArticles)
+    // );
+  });
+});
