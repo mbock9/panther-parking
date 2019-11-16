07c3785703432568cf8537478b59ccdfd0a97a3c
aumitleon <aumitleon1@gmail.com>
2019-11-16 01:00:08 -0500

    feat: develop form with material components

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,32 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import 'date-fns';
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker
+} from '@material-ui/pickers';
 import logo from '../Panther_Parking_logo-removebg-preview.png';
-const moment = require('moment');
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 
-const DropDownSelect = styled.select`
-  font-size: 12px;
-`;
+// Define styles for material-ui components
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300
+  },
+  root: {
+    flexGrow: 1
+  },
+  logo: {
+    maxWidth: 50,
+    transform: 'rotate(-30deg)'
+  }
+}));
 
-const Container = styled.div`
-  margin-left: 70px
-  width: 90%;
-  height: 40px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 10px;
-  padding-right: 10px;
-  color: white;
-  background: rgb(10, 100, 100);
-  display: flex;
-  justify-content: space-around;
-  flex-items: margin-left;
-  text-align: center;
-`;
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250
+    }
+  }
+};
 
-const View = styled.div`
-  background: rgb(10, 100, 100);
-`;
+const names = ['sPass', 'ePass', 'pPass', 'tPass', 'uPass'];
+
+const users = ['Student', 'Admin', 'Visitor'];
 
 const Form = ({
   permitType,
@@ -36,165 +57,140 @@ const Form = ({
   timeIn,
   setTimeIn,
   timeOut,
-  setTimeOut
+  setTimeOut,
+  date,
+  setDate
 }) => {
-  /*
-  For testing purposes
-  console.log("permit type:", permitType);
-  console.log("user type:", userType);
-  console.log("timein:", timeIn);
-  console.log("timeout:", timeOut);
-  */
+  // Instantiate style classes
+  const classes = useStyles();
+  // Testing
+  // console.log("permit type:", permitType);
+  // console.log("user type:", userType);
+  // console.log("timein:", timeIn);
+  // console.log("timeout:", timeOut);
 
-  // Generate an array of times
-  const interval = 30; // minutes interval
-  const times = []; // time array
-  let startTime = 0; // start time
-  const ap = ['AM', 'PM']; // AM-PM
+  // Define handlers for date and timeIn and timeOut changes.
+  const handleDateChange = date => {
+    if (Object.prototype.toString.call(date) === '[object Date]') {
+      setDate(date);
+    }
+  };
 
-  // Loop to increment the time and push results in array
-  for (let i = 0; startTime < 24 * 60; i++) {
-    const hours = Math.floor(startTime / 60); // getting hours of day in 0-24 format
-    const minutes = startTime % 60; // getting minutes of the hour in 0-55 format
-    times[i] = `${`${hours === 12 || hours === 0 ? 12 : hours % 12}`.slice(
-      -2
-    )}:${`0${minutes}`.slice(-2)}${ap[Math.floor(hours / 12)]}`; // pushing data in array in [00:00 - 12:00 AM/PM format]
-    startTime = startTime + interval;
-  }
+  const handleTimeInChange = time => {
+    if (Object.prototype.toString.call(time) === '[object Date]') {
+      setTimeIn(time);
+    }
+  };
 
-  // Create option elements for each time in the array
-  const timeInOptions = times.map(time => (
-    <option key={time} value={time}>
-      {time}
-    </option>
-  ));
-
-  // Filter times array based on timeIn
-  // Edge case: if timeIn is 11:30PM, then filtered array is empty.
-  const timesOutArr = times.filter(
-    time =>
-      Date.parse(moment(time, 'hh:mm A')) >
-      Date.parse(moment(timeIn, 'hh:mm A'))
-  );
-
-  // Create timeout options
-  const timeOutOptions = timesOutArr.map(time => (
-    <option key={time} value={time}>
-      {time}
-    </option>
-  ));
+  const handleTimeOutChange = time => {
+    if (Object.prototype.toString.call(time) === '[object Date]') {
+      setTimeOut(time);
+    }
+  };
 
   return (
-    <View>
-      <Container>
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          style={{ height: '80px', marginTop: '-15px' }}
-        />
-        <h4
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '15px',
-            marginRight: '-20px'
-          }}
-        >
-          Permit type:
-        </h4>
-        <DropDownSelect
-          value={permitType}
-          onChange={event => {
-            setPermit(event.target.value);
-          }}
-        >
-          <option value="sPass">PermitS</option>
-          <option value="pPass">PermitP</option>
-          <option value="uPass">PermitU</option>
-          <option value="ePass">PermitE</option>
-          <option value="tPass">PermitT</option>
-        </DropDownSelect>
-
-        <h4
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '15px',
-            marginRight: '-20px'
-          }}
-        >
-          User type:
-        </h4>
-
-        <DropDownSelect
-          value={userType}
-          onChange={event => {
-            setUser(event.target.value);
-          }}
-        >
-          <option value="admin">Admin</option>
-          <option value="student">Student</option>
-          <option value="visitor">Visitor</option>
-        </DropDownSelect>
-
-        <h4
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '15px',
-            marginRight: '-20px'
-          }}
-        >
-          Time in:
-        </h4>
-
-        <DropDownSelect
-          value={timeIn}
-          onChange={event => {
-            setTimeIn(event.target.value);
-          }}
-        >
-          {timeInOptions}
-        </DropDownSelect>
-
-        <h4
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '15px',
-            marginRight: '-20px'
-          }}
-        >
-          Time out:
-        </h4>
-
-        <DropDownSelect
-          value={timeOut}
-          onChange={event => {
-            setTimeOut(event.target.value);
-          }}
-        >
-          {timeOutOptions}
-        </DropDownSelect>
-      </Container>
-    </View>
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static" color="inherit">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            ></IconButton>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Grid container justify="space-around" spacing={1}>
+                <Toolbar>
+                  <img
+                    src={logo}
+                    className="App-logo"
+                    alt="logo"
+                    className={classes.logo}
+                  />
+                </Toolbar>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-mutiple-name-label">Permit</InputLabel>
+                  <Select
+                    labelId="demo-mutiple-name-label"
+                    id="demo-mutiple-name"
+                    value={permitType}
+                    onChange={event => {
+                      setPermit(event.target.value);
+                    }}
+                    input={<Input />}
+                    MenuProps={MenuProps}
+                  >
+                    {names.map(name => (
+                      <MenuItem key={name} value={name}>
+                        {name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-mutiple-name-label">
+                    User type
+                  </InputLabel>
+                  <Select
+                    labelId="demo-mutiple-name-label"
+                    id="demo-mutiple-name"
+                    value={userType}
+                    onChange={event => {
+                      setUser(event.target.value);
+                    }}
+                    input={<Input />}
+                    MenuProps={MenuProps}
+                  >
+                    {users.map(name => (
+                      <MenuItem key={name} value={name}>
+                        {name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-inline"
+                  label="Date"
+                  value={date}
+                  onChange={handleDateChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date'
+                  }}
+                />
+                <KeyboardTimePicker
+                  margin="normal"
+                  id="time-picker"
+                  label="Time In"
+                  mask="__:__ _M"
+                  value={timeIn}
+                  onChange={handleTimeInChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change time'
+                  }}
+                />
+                <KeyboardTimePicker
+                  margin="normal"
+                  id="time-picker"
+                  label="Time Out"
+                  mask="__:__ _M"
+                  value={timeOut}
+                  onChange={handleTimeOutChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change time'
+                  }}
+                />
+              </Grid>
+            </MuiPickersUtilsProvider>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </div>
   );
-};
-
-Form.propTypes = {
-  permitType: PropTypes.string.isRequired,
-  setPermit: PropTypes.func.isRequired,
-  userType: PropTypes.string.isRequired,
-  setUser: PropTypes.func.isRequired,
-  timeIn: PropTypes.string.isRequired,
-  setTimeIn: PropTypes.func.isRequired,
-  timeOut: PropTypes.string.isRequired,
-  setTimeOut: PropTypes.func.isRequired
 };
 
 export default Form;
