0c175c3d3f1b13fda267850a5e3a7615cbc42ae4
ben vandenbosch <benjaminrvandenbosch@gmail.com>
2019-11-29 18:18:22 -0500

    server/database test environment progress

--- server/server.test.js
+++ server/server.test.js
@@ -1,5 +1,38 @@
 const request = require('supertest');
 const { app } = require('./server');
+const MongodbMemoryServer = require('mongodb-memory-server').default;
+const { MongoClient } = require('mongodb');
+
+let mongoServer;
+let db;
+
+beforeAll(() => {
+  mongoServer = new MongodbMemoryServer();
+  // By return a Promise, Jest won't proceed with tests until the Promise
+  // resolves
+  return mongoServer
+    .getConnectionString()
+    .then(mongoURL => {
+      return Promise.all([
+        MongoClient.connect(mongoURL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }),
+        mongoServer.getDbName()
+      ]);
+    })
+    .then(([connection, dbName]) => {
+      db = connection.db(dbName);
+      app.locals.db = db; // Set db in app.js
+    })
+    .then(() => {
+      db.collection('parkingLots').createIndex({ title: 1 }, { unique: true });
+    });
+});
+
+afterAll(() => {
+  mongoServer.stop();
+});
 
 test('Server "smoke" test', () => {
   expect(app).toBeDefined();
@@ -20,11 +53,13 @@ test('GET /api/map/keys should return the key to the client', () => {
     });
 });
 
-test('GET /api/filter/:userType/:timeIn/:timeOut timeOut must be after timeIn', () => {
-  const firstDate = new Date('2019-11-29T22:39:37+0000');
-  const secondDate = new Date('2019-11-30T22:40:51+0000');
-  const userType = 'Visitor';
-  return request(app)
-    .get(`api/filter/${userType}/${secondDate}/${firstDate}`)
-    .expect(400);
+describe('Filtering endpoint', () => {
+  test('GET /api/filter/:userType/:timeIn/:timeOut timeOut must be after timeIn', () => {
+    const firstDate = new Date('2019-11-29T22:39:37+0000');
+    const secondDate = new Date('2019-11-30T22:40:51+0000');
+    const userType = 'Visitor';
+    return request(app)
+      .get(`api/filter/${userType}/${secondDate}/${firstDate}`)
+      .expect(400);
+  });
 });
