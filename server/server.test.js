8d20855c44a86b2377f1f4564361a9c7d5db6c47
aumitleon <aumitleon1@gmail.com>
2019-11-29 22:02:20 -0500

    tests: add tests for filter route test

--- server/server.test.js
+++ server/server.test.js
@@ -6,6 +6,64 @@ const { MongoClient } = require('mongodb');
 let mongoServer;
 let db;
 
+const parkingLots = [
+  {
+    type: 'Feature',
+    properties: {
+      lot_type: 'U',
+      name: 'Fitness Center',
+      description:
+        'Non-restricted faculty and staff lot outside of the fitness center and indoor tennis courts.',
+      permits: ['f/s']
+    },
+    geometry: {
+      coordinates: [
+        [
+          [-73.178348, 44.004339],
+          [-73.17822, 44.004277],
+          [-73.177428, 44.004985],
+          [-73.177557, 44.005047],
+          [-73.178348, 44.004339]
+        ]
+      ],
+      type: 'Polygon'
+    },
+    id: '0ac36e0587f719548969bfe77c7839b0'
+  },
+  {
+    type: 'Feature',
+    properties: {
+      lot_type: 'T',
+      name: 'Field House Lot',
+      description:
+        'Multi-purpose lot positioned at the end of the Peterson Family Athletics Complex.',
+      permits: [
+        'sPass',
+        'pPass',
+        'uPass',
+        'ePass',
+        'tPass',
+        'visitors',
+        'f/s',
+        'f/s_r'
+      ]
+    },
+    geometry: {
+      coordinates: [
+        [
+          [-73.180101, 44.00249],
+          [-73.179423, 44.00306],
+          [-73.178643, 44.002628],
+          [-73.178816, 44.00248],
+          [-73.180101, 44.00249]
+        ]
+      ],
+      type: 'Polygon'
+    },
+    id: '0e4b8a62b0830fe988fef86e62713e34'
+  }
+];
+
 beforeAll(() => {
   mongoServer = new MongodbMemoryServer();
   // By return a Promise, Jest won't proceed with tests until the Promise
@@ -26,7 +84,7 @@ beforeAll(() => {
       app.locals.db = db; // Set db in app.js
     })
     .then(() => {
-      db.collection('parkingLots').createIndex({ title: 1 }, { unique: true });
+      db.collection('parkingLots').createIndex({ type: 1 }, { unique: true });
     });
 });
 
@@ -55,11 +113,23 @@ test('GET /api/map/keys should return the key to the client', () => {
 
 describe('Filtering endpoint', () => {
   test('GET /api/filter/:userType/:timeIn/:timeOut timeOut must be after timeIn', () => {
-    const firstDate = new Date('2019-11-29T22:39:37+0000');
-    const secondDate = new Date('2019-11-30T22:40:51+0000');
+    const firstDate =
+      'Fri-Nov-29-2019-21:57:51-GMT-0500-(Eastern-Standard-Time)';
+    const secondDate =
+      'Fri-Nov-29-2019-22:57:51-GMT-0500-(Eastern-Standard-Time)';
     const userType = 'Visitor';
     return request(app)
-      .get(`api/filter/${userType}/${secondDate}/${firstDate}`)
+      .get(`/api/map/filter/${userType}/${secondDate}/${firstDate}`)
       .expect(400);
+  });
+  test('GET /api/filter/:userType/:timeIn/:timeOut should return json object.', () => {
+    const firstDate =
+      'Fri-Nov-29-2019-21:57:51-GMT-0500-(Eastern-Standard-Time)';
+    const secondDate =
+      'Fri-Nov-29-2019-22:57:51-GMT-0500-(Eastern-Standard-Time)';
+    const userType = 'Student-sPass';
+    return request(app)
+      .get(`/api/map/filter/${userType}/${firstDate}/${secondDate}`)
+      .expect(200);
   });
 });
