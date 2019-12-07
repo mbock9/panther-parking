2aaa3c633d049380b88f9a564caf0e9995274f93
Miles Bock <mbock@middlebury.edu>
2019-12-07 13:30:53 -0500

    added smoke test and mock fetches

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { mount, shallow } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 
 import App from './App';
 import Form from './components/Form';
 import ParkingMap from './components/ParkingMap';
+
+const parkingLots = [
+  {
+    type: 'Feature',
+    properties: {
+      lot_type: 'U',
+      name: 'Fitness Center',
+      description:
+        'Non-restricted faculty and staff lot outside of the fitness center and indoor tennis courts.',
+      permits: ['f/s']
+    },
+    geometry: {
+      coordinates: [
+        [
+          [-73.178348, 44.004339],
+          [-73.17822, 44.004277],
+          [-73.177428, 44.004985],
+          [-73.177557, 44.005047],
+          [-73.178348, 44.004339]
+        ]
+      ],
+      type: 'Polygon'
+    },
+    id: '0ac36e0587f719548969bfe77c7839b0'
+  },
+  {
+    type: 'Feature',
+    properties: {
+      lot_type: 'T',
+      name: 'Field House Lot',
+      description:
+        'Multi-purpose lot positioned at the end of the Peterson Family Athletics Complex.',
+      permits: [
+        'sPass',
+        'pPass',
+        'uPass',
+        'ePass',
+        'tPass',
+        'visitors',
+        'f/s',
+        'f/s_r'
+      ]
+    },
+    geometry: {
+      coordinates: [
+        [
+          [-73.180101, 44.00249],
+          [-73.179423, 44.00306],
+          [-73.178643, 44.002628],
+          [-73.178816, 44.00248],
+          [-73.180101, 44.00249]
+        ]
+      ],
+      type: 'Polygon'
+    },
+    id: '0e4b8a62b0830fe988fef86e62713e34'
+  }
+];
+
+function flushPromises() {
+  return new Promise(resolve => setImmediate(resolve));
+}
+
+const mockResponse = data =>
+  Promise.resolve({ ok: true, json: () => Promise.resolve(data) });
+
+const mockFetch = (url, options) => {
+  if (options) {
+    if (options.method === 'PUT') {
+      // we don't store any changes, we just return the same object
+      const data = JSON.parse(options.body);
+      return mockResponse(data);
+    }
+  } else {
+    return mockResponse(parkingLots);
+  }
+};
 
 describe('App shallow rendering tests', () => {
   let app;
@@ -30,6 +108,22 @@ describe('App shallow rendering tests', () => {
     test('ParkingMap has PropTypes defined', () => {
       expect(ParkingMap).toHaveProperty('propTypes');
     });
+  });
+});
+
+describe('App test', () => {
+  beforeAll(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+  });
+
+  afterAll(() => {
+    global.fetch.mockClear();
+  });
+
+  test('Smoke test', async () => {
+    const comp = mount(<App />);
+    await act(async () => await flushPromises());
+    comp.update();
   });
 });
 
