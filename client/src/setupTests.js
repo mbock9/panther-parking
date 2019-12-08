255707646ef1338873b9f65b3ec9dc64748c4215
Miles Bock <mbock@middlebury.edu>
2019-12-08 15:43:48 -0500

    cleanup

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -72,64 +72,6 @@ export const mockResp = {
   }
 };
 
-export const parkingLots = [
-  {
-    type: 'Feature',
-    properties: {
-      lot_type: 'U',
-      name: 'Fitness Center',
-      description:
-        'Non-restricted faculty and staff lot outside of the fitness center and indoor tennis courts.',
-      permits: ['f/s']
-    },
-    geometry: {
-      coordinates: [
-        [
-          [-73.178348, 44.004339],
-          [-73.17822, 44.004277],
-          [-73.177428, 44.004985],
-          [-73.177557, 44.005047],
-          [-73.178348, 44.004339]
-        ]
-      ],
-      type: 'Polygon'
-    },
-    id: '0ac36e0587f719548969bfe77c7839b0'
-  },
-  {
-    type: 'Feature',
-    properties: {
-      lot_type: 'T',
-      name: 'Field House Lot',
-      description:
-        'Multi-purpose lot positioned at the end of the Peterson Family Athletics Complex.',
-      permits: [
-        'sPass',
-        'pPass',
-        'uPass',
-        'ePass',
-        'tPass',
-        'visitors',
-        'f/s',
-        'f/s_r'
-      ]
-    },
-    geometry: {
-      coordinates: [
-        [
-          [-73.180101, 44.00249],
-          [-73.179423, 44.00306],
-          [-73.178643, 44.002628],
-          [-73.178816, 44.00248],
-          [-73.180101, 44.00249]
-        ]
-      ],
-      type: 'Polygon'
-    },
-    id: '0e4b8a62b0830fe988fef86e62713e34'
-  }
-];
-
 const dateProp = new Date();
 
 export const testCase1 = {
@@ -138,15 +80,6 @@ export const testCase1 = {
   timeOut: dateProp,
   mobileOpen: false,
   landingPage: true,
-  lotSelected: 'false'
-};
-
-export const testCase2 = {
-  userType: 'Visitor',
-  timeIn: dateProp,
-  timeOut: dateProp,
-  mobileOpen: false,
-  landingPage: false,
   lotSelected: 'false'
 };
 
