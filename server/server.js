3898658e4ba3d106fc83e6a1e4e659fc7c8a048b
ben vandenbosch <benjaminrvandenbosch@gmail.com>
2019-12-08 13:15:07 -0500

    initial endpoint logic

--- server/server.js
+++ server/server.js
@@ -97,64 +97,45 @@ app.get(
   }
 );
 
-// app.get(
-//   '/api/map/filter/:userType/:timeIn/:timeOut/:lotSelected',
-//   (request, response, next) => {
-//     // Reformat date strings
-//     const timeIn = new Date(request.params.timeIn.replace(/-+/g, ' '));
-//     const timeOut = new Date(request.params.timeOut.replace(/-+/g, ' '));
-//     const { userType } = request.params;
-//
-//     // Validations
-//     if (!validUsers.includes(userType)) {
-//       response.sendStatus(400);
-//       return;
-//     }
-//     if (Number.isNaN(Date.parse(timeIn)) || Number.isNaN(Date.parse(timeOut))) {
-//       response.sendStatus(400);
-//       return;
-//     }
-//     if (timeOut.getTime() < timeIn.getTime()) {
-//       response.sendStatus(400);
-//       return;
-//     }
-//
-//     // Account for time change from UTC to EST
-//     timeIn.setHours(timeIn.getHours() + 5);
-//     timeOut.setHours(timeOut.getHours() + 5);
-//
-//     const parkableQuery = utils.constructQuery(timeIn, timeOut, userType);
-//     const nonparkableQuery =
-//       parkableQuery.$or === undefined
-//         ? { type: 'Not feature' }
-//         : { $nor: parkableQuery.$or };
-//
-//     let parkable;
-//     let nonparkable;
-//
-//     app.locals.db
-//       .collection('parkingLots')
-//       .find(parkableQuery)
-//       .toArray()
-//       .then(documents => {
-//         parkable = {
-//           features: documents,
-//           type: 'FeatureCollection'
-//         };
-//         app.locals.db
-//           .collection('parkingLots')
-//           .find(nonparkableQuery)
-//           .toArray()
-//           .then(docs => {
-//             nonparkable = {
-//               features: docs,
-//               type: 'FeatureCollection'
-//             };
-//             response.send({ parkable, nonparkable });
-//           }, next); // Use "next" as rejection handler
-//       }, next); // Use "next" as rejection handler
-//   }
-// );
+app.get(
+  '/api/map/filter/:userType/:timeIn/:timeOut/:lotSelected',
+  (request, response, next) => {
+    // Reformat date strings
+    const timeIn = new Date(request.params.timeIn.replace(/-+/g, ' '));
+    const timeOut = new Date(request.params.timeOut.replace(/-+/g, ' '));
+    const { userType } = request.params;
+
+    // Validations
+    if (!isFilterArgsValid(timeIn, timeOut, userType)) {
+      response.sendStatus(400);
+      return;
+    }
+
+    // Account for time change from UTC to EST
+    timeIn.setHours(timeIn.getHours() + 5);
+    timeOut.setHours(timeOut.getHours() + 5);
+
+    // If lotSelected isn't false, query only for lot name
+    let query = '';
+    if (lotSelected === 'false') {
+      query = utils.constructQuery(timeIn, timeOut, userType);
+    } else {
+      query = { 'properties.name': lotSelected };
+    }
+
+    app.locals.db
+      .collection('parkingLots')
+      .find(query)
+      .toArray()
+      .then(documents => {
+        let sidebarLots = documents.map(lot => ({
+          name: lot.properties.name,
+          description: lot.properties.description
+        }));
+        response.send(sidebarLots);
+      }, next); // Use "next" as rejection handler
+  }
+);
 
 // A very simple error handler. In a production setting you would
 // not want to send information about the inner workings of your
